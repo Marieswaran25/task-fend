1003d7f0225df61893dcfdebae173a459fe90ad9
Marieswaran01 <marieswaran@next99.com>
2025-03-28 01:08:49 +0530

    fix: rename pages directory

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import { View } from '@library/View';
 
-import { HomeLayout } from '@/pages/home/home';
+import { HomeLayout } from '@/layouts/home';
 import StoreProvider from '@/provider/storeProvider';
 
 export default function HomePage() {

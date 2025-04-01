0031273de301e597daaa87448df3ac1670c05958
Marieswaran01 <marieswaran@next99.com>
2025-04-01 11:52:12 +0530

    feat: Add analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import './globals.scss';
 
+import { Analytics } from '@vercel/analytics/react';
 import { Metadata } from 'next';
 import { Roboto } from 'next/font/google';
 
@@ -23,7 +24,10 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en">
-            <body className={roboto.className}>{children}</body>
+            <body className={roboto.className}>
+                {children}
+                <Analytics />
+            </body>
         </html>
     );
 }
